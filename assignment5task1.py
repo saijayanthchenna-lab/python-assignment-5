9716b336fabcda27d663308c80598e86b94b38b6
saijayanthchenna <saijayanthchenna@gmail.com>
2026-02-05 20:21:52 +0530

    "Intial commit"

---
+++ assignment5task1.py
@@ -0,0 +1,3 @@
+stud_dict={'John':67,'Jane':68,'Alice':97,'Robin':64,'Henry':75}
+key=input("Enter student's name: ")
+print(f"{key}'s marks: {stud_dict[key]}")
---
+++ assignment5task2.py
@@ -0,0 +1,5 @@
+num=[1,2,3,4,5,6,7,8,9,10]
+print(f"Original list: {num}")
+print(f"Extracted first five elements: {num[:5]}")
+num1=num[:5]
+print(f"Reversed extracted elements: {num1[::-1]}")
